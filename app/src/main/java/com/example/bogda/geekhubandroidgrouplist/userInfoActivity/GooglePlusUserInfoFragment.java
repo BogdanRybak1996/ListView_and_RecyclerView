80161169e48c95298f2479de3e10df773ef3cd83
BogdanRybak <bogdanrybak1996@gmail.com>
2016-11-05 00:21:55 +0200

    Version with bugs and bad design

--- app/src/main/java/com/example/bogda/geekhubandroidgrouplist/userInfoActivity/GooglePlusUserInfoFragment.java
+++ app/src/main/java/com/example/bogda/geekhubandroidgrouplist/userInfoActivity/GooglePlusUserInfoFragment.java
@@ -1,5 +1,9 @@
 package com.example.bogda.geekhubandroidgrouplist.userInfoActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,9 +11,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.bogda.geekhubandroidgrouplist.R;
 import com.example.bogda.geekhubandroidgrouplist.data.GooglePlusUser.GooglePlusUser;
+import com.example.bogda.geekhubandroidgrouplist.data.GooglePlusUser.Organization;
+import com.example.bogda.geekhubandroidgrouplist.data.GooglePlusUser.Place;
 import com.google.gson.Gson;
 import com.squareup.picasso.Picasso;
 
@@ -30,20 +38,117 @@ public class GooglePlusUserInfoFragment extends Fragment {
         super.onStart();
         String json = getActivity().getIntent().getStringExtra("json");
         Gson gson = new Gson();
-        GooglePlusUser user = gson.fromJson(json,GooglePlusUser.class);
+        GooglePlusUser user = gson.fromJson(json, GooglePlusUser.class);
         user.getBirthday();
 
         //Cover photo
-        if(user.getCover() != null) {
+        if (user.getCover() != null) {
             ImageView coverImageView = (ImageView) getActivity().findViewById(R.id.google_plus_user_info_image_background);
             Picasso.with(getActivity()).load(user.getCover().getCoverPhoto().getUrl()).into(coverImageView);
         }
 
         //Main photo
-        if(user.getImage() != null) {
+        if (user.getImage() != null) {
             ImageView mainImageView = (ImageView) getActivity().findViewById(R.id.google_plus_user_info_image_main);
-            String url = user.getImage().getUrl().replaceFirst("sz=50","sz=200");
+            String url = user.getImage().getUrl().replaceFirst("sz=50", "sz=200");
             Picasso.with(getActivity()).load(url).into(mainImageView);
         }
+
+        //Name
+        TextView nameTextView = (TextView) getActivity().findViewById(R.id.google_plus_user_info_name);
+        nameTextView.setText(user.getDisplayName());
+
+        //Click on google+ logo
+        ImageView googleLogoImageView = (ImageView) getActivity().findViewById(R.id.google_plus_logo_image);
+        final Uri url = Uri.parse(user.getUrl());
+        googleLogoImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(url);
+                startActivity(intent);
+            }
+        });
+
+        //Birthday
+        TextView birthdayTextViewLabel = (TextView) getActivity().findViewById(R.id.google_plus_user_info_birthday_label);
+        TextView birthdayTextView = (TextView) getActivity().findViewById(R.id.google_plus_user_info_birthday);
+        if (user.getBirthday() != null) {
+            birthdayTextView.setText(user.getBirthday());
+        } else {
+            birthdayTextViewLabel.setVisibility(View.GONE);
+            birthdayTextView.setVisibility(View.GONE);
+        }
+
+        //Gender
+        TextView genderTextViewLabel = (TextView) getActivity().findViewById(R.id.google_plus_user_info_gender_label);
+        TextView genderTextView = (TextView) getActivity().findViewById(R.id.google_plus_user_info_gender);
+        if (user.getGender() != null) {
+            genderTextView.setText(user.getGender());
+        } else {
+            genderTextViewLabel.setVisibility(View.GONE);
+            genderTextView.setVisibility(View.GONE);
+        }
+
+        //Organizations
+        TextView organizationsTextViewLabel = (TextView) getActivity().findViewById(R.id.google_plus_user_info_organizations_label);
+        LinearLayout organizationsLayout = (LinearLayout) getActivity().findViewById(R.id.google_plus_organizations_layout);
+        organizationsLayout.setPadding(4,4,4,4);
+        organizationsLayout.removeAllViews();
+        if (user.getOrganizations() != null) {
+            int counter = 1;
+            for (Organization o : user.getOrganizations()) {
+                TextView textView = new TextView(getActivity());
+                textView.setText(Integer.toString(counter)+") " + o.getName());
+                Paint p = new Paint();
+                p.setColor(Color.BLUE);
+                textView.setPaintFlags(p.getColor());
+                textView.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+                final Organization
+                        tempOrganization = o;
+                textView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse("geo:0,0?q="+tempOrganization.getName()));
+                        startActivity(intent);
+                    }
+                });
+                organizationsLayout.addView(textView);
+            }
+        } else {
+            organizationsLayout.setVisibility(View.GONE);
+            organizationsTextViewLabel.setVisibility(View.GONE);
+        }
+
+
+        //Places
+        TextView placesTextViewLabel = (TextView) getActivity().findViewById(R.id.google_plus_user_info_places_label);
+        LinearLayout placesLayout = (LinearLayout) getActivity().findViewById(R.id.google_plus_places_layout);
+        placesLayout.setPadding(4,4,4,4);
+        placesLayout.removeAllViews();
+        if (user.getPlacesLived() != null) {
+            int counter = 1;
+            for (Place o : user.getPlacesLived()) {
+                TextView textView = new TextView(getActivity());
+                textView.setText(Integer.toString(counter)+") " + o.getValue());
+                Paint p = new Paint();
+                p.setColor(Color.BLUE);
+                textView.setPaintFlags(p.getColor());
+                textView.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+                final Place tempPlace = o;
+                textView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse("geo:0,0?q="+tempPlace.getValue()));
+                        startActivity(intent);
+                    }
+                });
+                placesLayout.addView(textView);
+            }
+        } else {
+            placesLayout.setVisibility(View.GONE);
+            placesTextViewLabel.setVisibility(View.GONE);
+        }
+
     }
 }
